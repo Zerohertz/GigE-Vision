50d42da7341aea96151f124970ca94e6b808afff
Zerohertz <ohg3417@gmail.com>
2022-01-08 01:41:24 +0900

    Add GUI | Edit Source

--- AcquisitionVisionData.m
+++ AcquisitionVisionData.m
@@ -8,7 +8,7 @@ g = gigecam('192.168.100.1')
 
 %% Setup Camera
 
-g.ExposureTime = 1000;
+g.ExposureTime = 2300;
 
 %%
 
@@ -17,3 +17,19 @@ preview(g)
 %%
 
 closePreview(g)
+
+%%
+
+% mkdir 20220107
+% cd 20220107
+
+% preview(g)
+
+i = 1;
+
+while true
+    img = snapshot(g);
+    imwrite(img, append(string(i),'.jpg'));
+    pause(1) % Sampling Rate [fps]
+    i = i + 1;
+end
